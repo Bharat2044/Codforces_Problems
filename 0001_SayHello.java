0dd4d747149a01dc92ef67e134fb3d0574ae4fc3
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-29 11:32:29 +0530

    code added

--- 0001_SayHello.java
+++ 0001_SayHello.java
@@ -6,7 +6,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-public class Main {
+public class SayHello {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
