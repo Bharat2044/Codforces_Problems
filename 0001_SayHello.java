6ea330bd6fc9e24c686339558188cede49d971ac
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-29 11:21:42 +0530

    code added

---
+++ 0001_SayHello.java
@@ -0,0 +1,19 @@
+// Question Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/A
+
+
+// TC = O(1), SC = O(1)
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String name = br.readLine();
+        
+        System.out.println("Hello, " + name);
+        
+        br.close();
+    }
+}
